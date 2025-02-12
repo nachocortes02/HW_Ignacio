457fc15466b760b4ac6cd8b8d2d938c897dfe927
Ignacio Cortes Niilus <nachocortes02@mac.mynetworksettings.com>
2025-02-12 16:25:41 -0500

    Fixed typo in mean parameter

--- Ignacio_hw.R
+++ Ignacio_hw.R
@@ -1,5 +1,3 @@
-i havrandom_numbers <- rnorm(100, mean = 25, sd = 1)
-random_numbers <- rnorm(100, nean = 25, sd = 1)
-
+random_numbers <- rnorm(100, mean = 25, sd = 1)
 
 mean_value <- mean(random_numbers)
