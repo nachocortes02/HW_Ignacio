a9ece4c27926a54861887a3f0b9178fa596e8390
Ignacio Cortes Niilus <nachocortes02@mac.mynetworksettings.com>
2025-02-12 16:23:17 -0500

    Compute the average

--- Ignacio_hw.R
+++ Ignacio_hw.R
@@ -1,4 +1,5 @@
-random_numbers <- rnorm(100, mean = 25, sd = 1)
+i havrandom_numbers <- rnorm(100, mean = 25, sd = 1)
+random_numbers <- rnorm(100, nean = 25, sd = 1)
 
-git add Ignacio_hw.R
 
+mean_value <- mean(random_numbers)
